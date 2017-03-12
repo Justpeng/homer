06579c9c35e290a98ac0cdc7047ffb2855d7ee9b
lipeng <lipeng_ly@163.com>
2017-03-12 09:27:35 +0800

    修改SrpingMVC配置，在DAO层添加@Repository注解，使得idea在spring插件可以在view和controller间跳转

--- homer-manage/homer-manage-service/src/main/java/com/just/code/manage/service/impl/SysUserServiceImpl.java
+++ homer-manage/homer-manage-service/src/main/java/com/just/code/manage/service/impl/SysUserServiceImpl.java
@@ -1,10 +1,10 @@
 package com.just.code.manage.service.impl;
 
 
+
 import com.just.code.manage.mapper.SysUserMapper;
 import com.just.code.manage.pojo.SysUser;
 import com.just.code.manage.service.SysUserService;
-
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -14,7 +14,6 @@ public class SysUserServiceImpl implements SysUserService {
 	
 	@Resource
 	private SysUserMapper sysUserMapper;
-
 
 	@Override
 	public SysUser getById(Long id) {
--- homer-manage/homer-manage-web/src/main/webapp/view/showUser.jsp
+++ homer-manage/homer-manage-web/src/main/webapp/view/showUser.jsp
@@ -1,5 +1,5 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="utf-8" %>
-<%@ include file="/WEB-INF/view/common/tagPage.jsp" %>
+<%@ include file="/view/common/tagPage.jsp" %>
 <!DOCTYPE html>
 <html lang="zh-CN">
 <head>
